01ea9811e9fbde886af20f5d88a67b3057a5a17c
Feltrin <cyril.feltrin@etu.univ-smb.fr>
2024-01-09 14:05:49 +0100

    Voiture mais qui ne bouge pas

--- Sae 1.01 1.02/MainWindow.xaml.cs
+++ Sae 1.01 1.02/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Sae_1._01_1._02
 {
@@ -21,32 +23,63 @@ namespace Sae_1._01_1._02
     public partial class MainWindow : Window
     {
         // booléens pour monter et descendre
-        private bool goUp, goDown = false;
+        bool goUp, goDown = false;
+        int vitesseJoueur = 10;
+        int vitesseEnnemi = 10;
 
-
+        private ImageBrush playerSkin = new ImageBrush();
 
         public MainWindow()
         {
             InitializeComponent();
-
-
+            // chargement de l’image du joueur 
+            playerSkin.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "formule1.png"));
+            // assignement de skin du joueur au rectangle associé
+            player1.Fill = playerSkin;
         }
 
         private void Canvas_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Up)
             {
-                g
+                goUp = true;
             }
             if (e.Key == Key.Down)
             {
-
+                goDown = true;
             }
         }
 
         private void Canvas_KeyUp(object sender, KeyEventArgs e)
         {
-            if(e.Key != Key.Up) { }
+            if (e.Key == Key.Up)
+            {
+                goUp = false;
+            }
+            if (e.Key == Key.Down)
+            {
+                goDown = false;
+            }
+        }
+
+        private void GameEngine(object sender, EventArgs e)
+        {
+            /*// création d'un rectangle joueur pour la détection de collision
+            Rect joueur = new Rect(Canvas.GetLeft(joueur1), Canvas.GetTop(joueur1), joueur1.Width, joueur1.Height);*/
+
+            Bouger_Joueur();
+        }
+
+        private void Bouger_Joueur()
+        {
+            if (goUp && Canvas.GetTop(player1) > 0)
+            {
+                Canvas.SetTop(player1, Canvas.GetTop(player1) - vitesseJoueur);
+            }
+            if (goDown && Canvas.GetTop(player1) > 0)
+            {
+                Canvas.SetTop(player1, Canvas.GetTop(player1) + vitesseJoueur);
+            }
         }
     }
 }
