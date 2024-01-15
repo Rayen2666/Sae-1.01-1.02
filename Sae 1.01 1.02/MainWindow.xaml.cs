a79f498527b2fd79aac493139450f5657543d2c4
Rayen Ben ahmed <Rayen.Ben-Ahmed@etu.univ-savoie.fr>
2024-01-15 08:25:19 +0100

    Version 3 hitbox

--- Sae 1.01 1.02/MainWindow.xaml.cs
+++ Sae 1.01 1.02/MainWindow.xaml.cs
@@ -35,6 +35,12 @@ namespace Sae_1._01_1._02
         Rect joueurHitBox;
         Rect bordure1HitBox;
         Rect bordure2HitBox;
+        Rect enemie1HitBox;
+        Rect enemie2HitBox;
+        Rect enemie3HitBox;
+        Rect enemie4HitBox;
+
+        bool gameover = false;
 
         private ImageBrush playerSkin = new ImageBrush();
 
@@ -44,9 +50,9 @@ namespace Sae_1._01_1._02
         ImageBrush enemie4Skin = new ImageBrush();
         ImageBrush ArrierePlanSprite = new ImageBrush();
         DispatcherTimer jeuTimer = new DispatcherTimer();
-        
-       
-        
+
+
+
 
         Random vitesse = new Random();
 
@@ -57,7 +63,7 @@ namespace Sae_1._01_1._02
 
         Random rand = new Random();
 
-        int[] enemie1Position = { 115, 120, 125};
+        int[] enemie1Position = { 115, 120, 125 };
 
         int totalEnemie = 0;
 
@@ -75,14 +81,13 @@ namespace Sae_1._01_1._02
             else if (fenetreDebut.DialogResult == true)
             {
                 difficulte.ShowDialog();
-                
+
             }
 
-            
-          
-            
-            
-            DebutJeu();
+
+
+
+
             // chargement de l’image du joueur 
             playerSkin.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "formule1.png"));
             // assignement de skin du joueur au rectangle associé
@@ -91,17 +96,17 @@ namespace Sae_1._01_1._02
             ArrierePlan.Fill = ArrierePlanSprite;
             ArrierePlan2.Fill = ArrierePlanSprite;
 
-                jeuTimer.Interval = TimeSpan.FromMilliseconds(17);
-                jeuTimer.Tick += Jeu;
-            
             jeuTimer.Interval = TimeSpan.FromMilliseconds(17);
-            jeuTimer.Tick += Jeu; 
-            DebutJeu();
+            jeuTimer.Tick += Jeu;
+                  
 
             vitesse1 = vitesse.Next(10, 30);
             vitesse2 = vitesse.Next(10, 30);
             vitesse3 = vitesse.Next(10, 30);
             vitesse4 = vitesse.Next(10, 30);
+
+            jeuTimer.Start();
+            DebutJeu();
         }
 
 
@@ -119,6 +124,7 @@ namespace Sae_1._01_1._02
             enemie3.Fill = enemie3Skin;
             enemie4Skin.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "enemie4.png"));
             enemie4.Fill = enemie4Skin;
+            gameover = false;
         }
 
 
@@ -161,10 +167,7 @@ namespace Sae_1._01_1._02
             Canvas.SetLeft(ArrierePlan2, Canvas.GetLeft(ArrierePlan2) - 10);
 
 
-            speed1 = random.Next(15, 25);
-            speed2 = random.Next(20, 25);
-            speed3 = random.Next(27, 32);
-            speed4 = random.Next(25, 35);
+
             Canvas.SetLeft(enemie1, Canvas.GetLeft(enemie1) - vitesse1);
             Canvas.SetLeft(enemie2, Canvas.GetLeft(enemie2) - vitesse2);
             Canvas.SetLeft(enemie3, Canvas.GetLeft(enemie3) - vitesse3);
@@ -175,7 +178,7 @@ namespace Sae_1._01_1._02
             Collision();
             /*MakeEnemies(10);*/
 
-            voiturePasse.Content = "Voiture passee : " + totalEnemie;
+            /*voiturePasse.Content = "Voiture passee : " + totalEnemie;*/
 
             // si l'obstacle dépasse -50 emplacement
             if (Canvas.GetLeft(enemie1) < -50)
@@ -189,119 +192,169 @@ namespace Sae_1._01_1._02
                 // ajouter 1 au score
                 /*score += 1;*/
             }
-        }
 
-            
-            Bouger_Joueur();
-            ArrierePlanEnMouvement();
-            /*CollisionHaut();*/
-            Collision();
-
-
-        }
-            
+                joueurHitBox = new  Rect (Canvas.GetLeft(joueur), Canvas.GetTop(joueur), joueur.Width, joueur.Height);
+                enemie1HitBox = new Rect(Canvas.GetLeft(enemie1), Canvas.GetTop(enemie1), enemie1.Width, enemie1.Height);
+                enemie2HitBox = new Rect(Canvas.GetLeft(enemie2), Canvas.GetTop(enemie2), enemie2.Width, enemie2.Height);
+                enemie3HitBox = new Rect(Canvas.GetLeft(enemie3), Canvas.GetTop(enemie3), enemie3.Width, enemie3.Height);
+                enemie4HitBox = new Rect(Canvas.GetLeft(enemie4), Canvas.GetTop(enemie4), enemie4.Width, enemie4.Height);
 
         
 
-        /*---------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-        /*---------------------------------------------------------------------↓↓↓↓↓↓-METHODES-↓↓↓↓↓↓--------------------------------------------------------------------*/
 
-        /*private void MakeEnemies(int limit)
-        {
-            int left = 0;
-            // on conserve le max d’ennemis
-            totalEnemie = limit;
-            for (int i = 0; i < limit; i++)
-            {
-                ImageBrush enemieSkin = new ImageBrush();
-                Rectangle newEnemie = new Rectangle
+            if (joueurHitBox.IntersectsWith(enemie1HitBox) || joueurHitBox.IntersectsWith(enemie2HitBox) ||
+                joueurHitBox.IntersectsWith(enemie3HitBox) || joueurHitBox.IntersectsWith(enemie4HitBox))
                 {
-                    Tag = "enemy",
-                    Height = 45,
-                    Width = 45,
-                    Fill = enemieSkin,
-                };
-                Canvas.SetTop(newEnemie, 30);
-                Canvas.SetLeft(newEnemie, left);
-                myCanvas.Children.Add(newEnemie);
-                left -= 60;
-
-                *//*// incrémente les images des ennemis (max 8)
-                enemyImages++;
-                if (enemyImages > 8)
-                    enemyImages = 1;
-                enemySkin.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "images/invader" + enemie1Skin + ".png"));*//*
-            }
-        }*/
-
-        private void NouveauEnemie(int limit)
-        {
-
-        }
-
-        private void Collision()
-        {
-            /*joueurHitBox = new Rect(Canvas.GetLeft(joueur), Canvas.GetTop(joueur), joueur.Width, joueur.Height);*/
-             
-            if (Canvas.GetTop(joueur) < 93)
-            {
-                Canvas.SetTop(joueur, Canvas.GetTop(joueur) + 10);
-            }
-            if (Canvas.GetTop(joueur) > 357)
-            {
-                Canvas.SetTop(joueur, Canvas.GetTop(joueur) - 10);
-            }
-
-        }
+                    gameover = true;
+                    jeuTimer.Stop();
+                }
         
+             
 
-
-        private void ArrierePlanEnMouvement()
-        {
-            // déplacez l'arrière-plan de 10 pixels vers la gauche à chaque tick (1 tick = 17ms)
-            Canvas.SetLeft(ArrierePlan, Canvas.GetLeft(ArrierePlan) - 5);
-            Canvas.SetLeft(ArrierePlan2, Canvas.GetLeft(ArrierePlan2) - 5);
-
-            // code de défilement de parallaxe pour c#
-            // le code ci-dessous fera défiler l'arrière-plan simultanément et le fera paraître sans fin
-            // vérifie le premier arrière-plan
-            // si la première position X de l'arrière-plan descend en dessous de -1435 pixels
-            if (Canvas.GetLeft(ArrierePlan) < -1435)
+            if (gameover == true)
             {
-                // positionne le premier arrière-plan derrière le deuxième arrière-plan
-                // ci-dessous, nous définissons les arrière-plans à gauche, à la position de largeur background2
-                Canvas.SetLeft(ArrierePlan, Canvas.GetLeft(ArrierePlan2) + ArrierePlan2.Width);
+
+                enemie1.Stroke = Brushes.Black;
+                enemie1.StrokeThickness = 1;
+                enemie2.Stroke = Brushes.Black;
+                enemie2.StrokeThickness = 1;
+                enemie3.Stroke = Brushes.Black;
+                enemie3.StrokeThickness = 1;
+                enemie4.Stroke = Brushes.Black;
+                enemie4.StrokeThickness = 1;
+
+                joueur.Stroke = Brushes.Red;
+                joueur.StrokeThickness = 1;
+              
             }
-            // on fait pareil pour le fond 2
-            // si la position X de l'arrière-plan 2 descend en dessous de -1435
-            if (Canvas.GetLeft(ArrierePlan2) < -1435)
+            else
             {
-                // positionne le deuxième arrière-plan derrière le premier arrière-plan
-                // ci-dessous, nous définissons la position gauche de l'arrière-plan 2 ou la position X sur la position de la largeur de l'arrière-plan
-                Canvas.SetLeft(ArrierePlan2, Canvas.GetLeft(ArrierePlan) + ArrierePlan.Width);
+    
+                joueur.StrokeThickness = 0;
+                enemie1.StrokeThickness = 0;
+                enemie2.StrokeThickness = 0;
+                enemie3.StrokeThickness = 0;
+                enemie4.StrokeThickness = 0;
             }
+
+
+
+
+
+            int dkdk = rand.Next(120, 320);
+
+        }
+            
+             
+
+
+
+
+
+
+
+
+
+
+            /*---------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+            /*---------------------------------------------------------------------↓↓↓↓↓↓-METHODES-↓↓↓↓↓↓--------------------------------------------------------------------*/
+
+            /*private void MakeEnemies(int limit)
+            {
+                int left = 0;
+                // on conserve le max d’ennemis
+                totalEnemie = limit;
+                for (int i = 0; i < limit; i++)
+                {
+                    ImageBrush enemieSkin = new ImageBrush();
+                    Rectangle newEnemie = new Rectangle
+                    {
+                        Tag = "enemy",
+                        Height = 45,
+                        Width = 45,
+                        Fill = enemieSkin,
+                    };
+                    Canvas.SetTop(newEnemie, 30);
+                    Canvas.SetLeft(newEnemie, left);
+                    myCanvas.Children.Add(newEnemie);
+                    left -= 60;
+
+                    *//*// incrémente les images des ennemis (max 8)
+                    enemyImages++;
+                    if (enemyImages > 8)
+                        enemyImages = 1;
+                    enemySkin.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "images/invader" + enemie1Skin + ".png"));*//*
+                }
+            }*/
+
+            /*private void NouveauEnemie(int limit)
+            {
+
+            }
+            */
+            private void Collision()
+            {
+                /*joueurHitBox = new Rect(Canvas.GetLeft(joueur), Canvas.GetTop(joueur), joueur.Width, joueur.Height);*/
+
+                if (Canvas.GetTop(joueur) < 93)
+                {
+                    Canvas.SetTop(joueur, Canvas.GetTop(joueur) + 10);
+                }
+                if (Canvas.GetTop(joueur) > 357)
+                {
+                    Canvas.SetTop(joueur, Canvas.GetTop(joueur) - 10);
+                }
+
+            }
+
+
+
+            private void ArrierePlanEnMouvement()
+            {
+                // déplacez l'arrière-plan de 10 pixels vers la gauche à chaque tick (1 tick = 17ms)
+                Canvas.SetLeft(ArrierePlan, Canvas.GetLeft(ArrierePlan) - 5);
+                Canvas.SetLeft(ArrierePlan2, Canvas.GetLeft(ArrierePlan2) - 5);
+
+                // code de défilement de parallaxe pour c#
+                // le code ci-dessous fera défiler l'arrière-plan simultanément et le fera paraître sans fin
+                // vérifie le premier arrière-plan
+                // si la première position X de l'arrière-plan descend en dessous de -1435 pixels
+                if (Canvas.GetLeft(ArrierePlan) < -1435)
+                {
+                    // positionne le premier arrière-plan derrière le deuxième arrière-plan
+                    // ci-dessous, nous définissons les arrière-plans à gauche, à la position de largeur background2
+                    Canvas.SetLeft(ArrierePlan, Canvas.GetLeft(ArrierePlan2) + ArrierePlan2.Width);
+                }
+                // on fait pareil pour le fond 2
+                // si la position X de l'arrière-plan 2 descend en dessous de -1435
+                if (Canvas.GetLeft(ArrierePlan2) < -1435)
+                {
+                    // positionne le deuxième arrière-plan derrière le premier arrière-plan
+                    // ci-dessous, nous définissons la position gauche de l'arrière-plan 2 ou la position X sur la position de la largeur de l'arrière-plan
+                    Canvas.SetLeft(ArrierePlan2, Canvas.GetLeft(ArrierePlan) + ArrierePlan.Width);
+                }
+            }
+
+            private void Bouger_Joueur()
+            {
+                if (goUp && Canvas.GetTop(joueur) > 0)
+                {
+                    Canvas.SetTop(joueur, Canvas.GetTop(joueur) - vitesseJoueur);
+                }
+                if (goDown && Canvas.GetTop(joueur) > 0)
+                {
+                    Canvas.SetTop(joueur, Canvas.GetTop(joueur) + vitesseJoueur);
+                }
+            }
+
+            /*---------------------------------------------------------------------↑↑↑↑↑↑-METHODES-↑↑↑↑↑↑--------------------------------------------------------------------*/
+            /*---------------------------------------------------------------------------------------------------------------------------------------------------------------*/
         }
 
-        private void Bouger_Joueur()
-        {
-            if (goUp && Canvas.GetTop(joueur) > 0)
-            {
-                Canvas.SetTop(joueur, Canvas.GetTop(joueur) - vitesseJoueur);
-            }
-            if (goDown && Canvas.GetTop(joueur) > 0)
-            {
-                Canvas.SetTop(joueur, Canvas.GetTop(joueur) + vitesseJoueur);
-            }
-        }
-
-        /*---------------------------------------------------------------------↑↑↑↑↑↑-METHODES-↑↑↑↑↑↑--------------------------------------------------------------------*/
-        /*---------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-    }
 
 
 
-
-}
+    } 
    
 
 
